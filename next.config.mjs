c32d0321a4a0d1f3163dd56a6eddfd6f27cfff2c
rastor65 <63560982+rastor65@users.noreply.github.com>
2025-08-15 19:52:59 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  // basePath: process.env.NODE_ENV === 'production' ? '/cafe-producciones' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/cafe-producciones' : '',
   images: { unoptimized: true },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
