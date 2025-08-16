f0711c32bbc7225dc19d82ba42a0a88b3d717e25
rastor65 <rdamianquintero@uniguajira.edu.co>
2025-08-15 19:10:03 -0500

    .

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,10 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/cafe-producciones/' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/cafe-producciones/' : '',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true
-  }
-}
+  basePath: process.env.NODE_ENV === 'production' ? '/cafe-producciones' : '',
+  images: { unoptimized: true },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+};
 
-export default nextConfig
+export default nextConfig;
