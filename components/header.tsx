6cae75159f83df8716d528b567901f0803f6abe7
rastor65 <rdamianquintero@uniguajira.edu.co>
2025-08-15 19:13:51 -0500

    .

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import { Menu, X, Phone, MessageCircle } from "lucide-react"
-import Image from "next/image"
+import Image from "next/image";
 
 export function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
